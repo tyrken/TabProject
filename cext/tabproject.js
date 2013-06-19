6714b323dfcb650db8be078ed64b3c67892be822
Tristan Keen <tristan.keen@gmail.com>
2013-06-19 01:22:39 +0100

    Further popup display javascript

--- cext/tabproject.js
+++ cext/tabproject.js
@@ -2,6 +2,10 @@ var TPM = (function () {
 
   var my = {};
   
+  function startsWith = function (input, prefix) {
+    return input.slice(0, prefix.length) == prefix;
+  };
+
   my.getMWOPH = function () {
     return chrome.bookmarks.MAX_WRITE_OPERATIONS_PER_HOUR;
   }
@@ -11,12 +15,58 @@ var TPM = (function () {
   }
 
   var baseBookMarkName= "TabProject";
-  my.getProjects = function () {
-    return ['Stuff', 'Other Stuff'];
+
+  my.ProjectPageBase = 'chrome-extension://__MSG_@@extensionid/tabproject.html?name=';
+  my.getProjectPageUrl = function (name) {
+    return my.ProjectPageBase+encodeURIComponent(name);
+  }
+  my.isProjectPageUrl = function (url) {
+    return startsWith(url, TPM.ProjectPageBase);
+  }
+
+  function getParameterByName(url, name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(url);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  }
+
+  my.scanTabsForProjects = function () {
+    var projects = [];
+    console.log('Starting scanTabs');
+    chrome.tabs.query({}, function (tabs) {
+      var curProject = null;
+      var length = arr.length,
+          tab = null;
+      for (var i = 0; i < length; i++) {
+        tabs = tabs[i];
+        if (tabs.index == 0) {
+          curProject = null;
+        }
+        if (isProjectPageUrl(tabs.url)) {
+          curProject = { name: getParameterByName(tabs.url, 'name'), tabDescs: [] };
+          projects.push(curProject);
+          console.log('FirstProjectTab', curProject);
+        } else if (curProject != null) {
+          var tabDesc = { title: tab.title, url: tab.url; favIconUrl: tab.favIconUrl };
+          curProject.tabDescs.push(tabDesc);
+          console.log('ProjectTab', tabDesc);
+        }
+      }      
+    });
+    console.log('Finished scanTabs');
+    return projects;
   }
 
   return my;
 }());
+
+var Scanner = (function (TPM) {
+  var my = {};
+
+
+
+  return my;
+}(TPM));
+
 
 var Popup = (function (TPM) {
   var my = {};
@@ -26,38 +76,46 @@ var Popup = (function (TPM) {
   }
 
   my.addNewProject = function () {
+  alert("add");
     var name = $('newProjectName').text();
     if (isBlank(name)) {
       message("You must enter a Project Name first!");
       return;
     }
-    var projectPageUrl = 'chrome-extension://__MSG_@@extensionid/tabproject.html?name='+encodeURIComponent(name);
+    var projectPageUrl = TPM.getProjectPageUrl(name);
     chrome.tabs.create({url:projectPageUrl});
   }
 
   my.init = function () {
+  alert("init");
+
     // TODO enable button on name !blank
     $('#newProjectButton').click( function() {
       my.addNewProject();
     });
+
+    var projects = TPM.scanTabsForProjects();
+    var items = ['<ul>'];
+    $.each(projects, function(i, project) {
+      items.push('<li>'+project.name+'</li><ul>');
+      $.each(project.tabDescs, function(j, tabDesc) {
+        items.push('<li>'+tabDesc.title+'</li>');
+      });
+      items.push('</ul>');
+    });  // close each()
+    items.push('</ul>');
+    $('#projectList').append( items.join('') );    
   }
 
   return my;
 }(TPM));
 
 
-var Scanner = (function (TPM) {
-  var my = {};
-
-  my.scanForUpdates = function () {
-    $("#mwoph").text(TPM.getMWOPH());
-    $("#mswopm").text(TPM.getMSWOPM());
-  }
-
-  return my;
-}(TPM));
-
-// Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  Popup.init();
+  alert("ONO");
+  if (TPM.isProjectPageUrl(window.location.search)) {
+
+  } else {
+    Popup.init();
+  }
 });
