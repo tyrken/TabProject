89ae7be39933e909096f57b45ce788fb1bde4827
Tristan Keen <tristan.keen@gmail.com>
2013-07-06 10:48:52 +0100

    Initial import of jasmine

--- cext/requirejs-config.js
+++ cext/requirejs-config.js
@@ -5,6 +5,9 @@ requirejs.config({
     'jquery': ['lib/jquery'],
     'bootstrap': ['lib/bootstrap'],
     'tabproject': ['js/tabproject']
+    'jasmine': ['lib/jasmine-1.3.1/jasmine']
+    'jasmine-html': ['lib/jasmine-1.3.1/jasmine-html']
+    'jasmine-gui': ['test/jasmine-gui']
     },
   shim: {
     /* Set bootstrap dependencies (just jQuery) */
